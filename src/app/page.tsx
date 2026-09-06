b9566c3198ab379cd795f2da218f863bf47aa326
CHINMAYA KUMAR DAS <chinmayakumardas2000@gmail.com>
2026-09-06 15:47:15 +0530

    footer finalized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import { Hero } from "@/components/home/hero";
-import { Services } from "@/components/home/services";
-import { VisualPanels } from "@/components/home/visual-panels";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Services />
-      <VisualPanels />
+      
     </>
   );
 }
